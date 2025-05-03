2b180067143189bb29c1c06d210f51da10cdab64
Isha Maheshwari <ishamaheshwari2003@gmail.com>
2025-05-03 08:01:35 +0000

    test: need argument in element.sh

--- element.sh
+++ element.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Let's Start"
+# PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Check if argument was passed
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
