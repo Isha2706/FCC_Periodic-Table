38b8faf7c1f5d4ad7baf87882f00f18df73bbe60
Isha Maheshwari <ishamaheshwari2003@gmail.com>
2025-05-03 08:05:57 +0000

    refactor: Check if argument in element.sh

--- element.sh
+++ element.sh
@@ -7,3 +7,12 @@ if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
+
+# Determine input type
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  CONDITION="atomic_number=$1"
+elif [[ $1 =~ ^[A-Z][a-z]?$ ]]; then
+  CONDITION="symbol='$1'"
+else
+  CONDITION="name='$1'"
+fi
